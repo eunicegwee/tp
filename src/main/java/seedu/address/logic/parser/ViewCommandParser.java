117ee5e54de593cdbc112fc54c624dc96fb26d59
tanjy10 <harrontan2003@gmail.com>
2026-03-13 00:21:23 +0800

    Fix Checkstyle violations

--- src/main/java/seedu/address/logic/parser/ViewCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ViewCommandParser.java
@@ -4,8 +4,16 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.ViewCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new ViewCommand object
+ */
 public class ViewCommandParser implements Parser<ViewCommand> {
 
+    /**
+     * Parses the given {@code String} of arguments in the context of the ViewCommand
+     * and returns an ViewCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
     @Override
     public ViewCommand parse(String args) throws ParseException {
 
